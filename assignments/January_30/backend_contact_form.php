8b41022a8fe1fa7f756142237749e45800dd44f5
Aatish Balla <aatishballa@gmail.com>
2018-02-04 13:04:48 -0600

    added stylesheet

--- assignments/January_30/backend_contact_form.php
+++ assignments/January_30/backend_contact_form.php
@@ -2,11 +2,15 @@
 <html>
 <head>
 <title>Contact form</title>
+<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 
 <body>
   <?php
-  echo "<h1> Hi {$_POST['name']} !</h1>";
+  echo "<h1 class='word'> Hi {$_POST['name']} !</h1>";
+  echo "<h1> Thannks for sending me a message. I'll get back to you at</h1>";
+  echo"<h1 class='word'>{$_POST['phone']}</h1>";
+  echo "<h1> within 3-5 business days! - Aatish</h1>";
   ?>
 </body>
 </html>
