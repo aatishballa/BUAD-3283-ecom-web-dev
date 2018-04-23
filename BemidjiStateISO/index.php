8aaad699a9867a210281aa36172c32ae0c65c513
Aatish Balla <aatishballa@gmail.com>
2018-04-23 09:54:40 -0500

    geochart

--- BemidjiStateISO/index.php
+++ BemidjiStateISO/index.php
@@ -18,15 +18,11 @@
     <!-- Custom styles for this template -->
     <link href="css/style.css" rel="stylesheet">
 
-    <!-- CSS Animations -->
-    <link href="css/animate.css" rel="stylesheet">
 
 	<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script type="text/javascript">
       google.charts.load('current', {
         'packages':['geochart'],
-        // Note: you will need to get a mapsApiKey for your project.
-        // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
         'mapsApiKey': 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY'
       });
       google.charts.setOnLoadCallback(drawRegionsMap);
